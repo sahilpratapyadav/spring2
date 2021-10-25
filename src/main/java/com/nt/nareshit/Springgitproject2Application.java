1952ec9fca86d11de359d313a1e1326b3c0ed692
abc <abc@example.com>
2021-10-25 10:35:31 +0530

    main method modifedd

--- src/main/java/com/nt/nareshit/Springgitproject2Application.java
+++ src/main/java/com/nt/nareshit/Springgitproject2Application.java
@@ -2,12 +2,13 @@ package com.nt.nareshit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Springgitproject2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springgitproject2Application.class, args);
+		   ConfigurableApplicationContext run=   SpringApplication.run(Springgitproject2Application.class, args);
 	}
 
 }
