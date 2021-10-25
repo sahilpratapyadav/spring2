7b0c3b2dd26082d44c1bdb4d099d8659bb759a13
abc <abc@example.com>
2021-10-25 09:51:41 +0530

    intital comti

---
+++ src/main/java/com/nt/nareshit/Springgitproject2Application.java
@@ -0,0 +1,13 @@
+package com.nt.nareshit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springgitproject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springgitproject2Application.class, args);
+	}
+
+}
